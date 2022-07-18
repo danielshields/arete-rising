614b530e211e2ce8ebe8ba1b75c2c0bd82d9299a
Dan Shields <dan@chips.nyc>
2022-07-18 15:33:11 -0700

    Static prototypes

---
+++ proto/js/src/scripts.js
@@ -0,0 +1,42 @@
+const hovItems = document.querySelectorAll(".hov");
+if (hovItems.length > 0) {
+	hovItems.forEach((el) => {
+		el.addEventListener("mouseenter", (el) => {
+			el.target.classList.add("hovered");
+		});
+		el.addEventListener("mouseleave", (el) => {
+			el.target.classList.remove("hovered");
+		});
+	});
+}
+
+gsap.registerPlugin(SplitText);
+var tl = gsap.timeline(),
+	mySplitText = new SplitText(".intro-block", { type: "words" }),
+	chars = mySplitText.chars;
+
+// console.log(mySplitText);
+// gsap.set($quote, { perspective: 400 });
+
+//kill any animations and set text back to its pre-split state
+function kill() {
+	tl.clear().time(0);
+	mySplitText.revert();
+}
+
+kill();
+mySplitText.split({ type: "words" });
+tl.from(mySplitText.words, {
+	duration: 1,
+	// scale: 4,
+	autoAlpha: 0,
+	// rotationX: -180,
+	// transformOrigin: "100% 50%",
+	ease: "back",
+	stagger: 0.15,
+});
+
+//revert the text back to its pre-split state
+// $("#revert").click(function () {
+// 	mySplitText.revert();
+// });
